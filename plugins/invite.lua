6014670c7b183b796436620ec91649ec84e3bc88
MaStEr FeRi <feri.sys13@Gmail.com>
2016-02-12 00:41:49 +0330

    Update invite.lua

--- plugins/invite.lua
+++ plugins/invite.lua
@@ -23,8 +23,8 @@ function run(msg, matches)
   if not is_momod(msg) then
     return
   end
-  --if not is_admin(msg) then -- For admins only !
-    --return 'Only admins can invite.'
+  --if not is_sudo(msg) then -- For sudo only !
+    --return 'Only Sudo can invite.'
   --end
 	local cbres_extra = {chatid = msg.to.id}
   local username = matches[1]
@@ -33,7 +33,10 @@ function run(msg, matches)
 end
 return {
     patterns = {
-      "^[!/]invite (.*)$"
+      "^[!/]invite (.*)$",
+      "^invite (.*)$",
+      "^[!/]inv (.*)$",
+      "^inv (.*)$",
     },
     run = run
 }
